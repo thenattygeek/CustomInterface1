e80cf10cef973c796838eb16385ea530d3f7d99a
Nic <Nic@DESKTOP-LTCMLDQ>
2017-03-02 14:00:28 -0500

    Add project files.

---
+++ CustomInterface1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
